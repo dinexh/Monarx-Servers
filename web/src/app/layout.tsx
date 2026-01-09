5081543b69163cab45ea1622d4b81225700edee7
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2026-01-09 15:56:53 +0530

    chore: Remove unused favicon and logo files, and update package dependencies to include @vercel/analytics and postcss

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistMono = Geist_Mono({
@@ -25,6 +26,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
